628b52beb73c8424bbf24201c66067d267177b19
WilsonMcC <82771488+WilsonMcC@users.noreply.github.com>
2021-05-02 19:31:12 -0400

    Add files via upload

--- Encrypter.py
+++ Encrypter.py
@@ -3,7 +3,9 @@
 
 
 #imports
-import tkinter.messagebox
+import time
+from time import strftime
+import tkinter.messagebox 
 from tkinter import *
 from tkinter import filedialog
 from cryptography.fernet import Fernet
@@ -12,7 +14,7 @@ from tkinter import messagebox
 
 #width and height
 root = Tk()
-HEIGHT = 300
+HEIGHT = 0
 WIDTH = 400
 canvas = Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
@@ -22,19 +24,22 @@ root.title("Wilson McCormack Final")
 
 
 
-    
-
-
-
-
-
 #Open file function
 
-def openfile():
+def openfile_encrypt():
     filename=filedialog.askopenfilename(title="select file")
     print(filename)
     if filename:
         encrypt_file(filename)
+
+def openfile_decrypt():
+    messagebox.showinfo( "Decrypt key", "Please select the file you want to decrypt")
+    filename=filedialog.askopenfilename(title="select file to decrypt")
+    print(filename)
+    if filename:
+        messagebox.showinfo( "Decrypt key", "Please select the key file")
+        keyname=filedialog.askopenfilename(title="Select the key for the file to decrypt")
+        decrypt_file(filename, keyname)
 
 def encrypt_file(filename):
     key = Fernet.generate_key()
@@ -50,36 +55,67 @@ def encrypt_file(filename):
     fernet = Fernet(key)
     encrypted = fernet.encrypt(original)
 
-    file_to_encrypt = open(filename, "wb")
+    file_to_encrypt = open(filename + "_Encrypted", "wb")
     file_to_encrypt.write(encrypted)
     file_to_encrypt.close()
-    messagebox.showinfo( "Hello Python", "File has been encrypted")
+    messagebox.showinfo( "Encrypt", "File has been encrypted")
 
 
 
-def decrypt_file():
-    pass
+def decrypt_file(filename, keyname):
+    key_contents = None
+    with open(keyname, "rb") as f:
+        key_contents = f.read()
+    try:
+        fernet = Fernet(key_contents)
+    except:
+        messagebox.showerror(title = "ERROR", message = "Please select a valid key file.")
+        return
+    file_to_decrypt = open(filename,"rb")
+    file_contents = file_to_decrypt.read()
+    file_to_decrypt.close()
 
-#Open file button
-fileButton = Button(root, text ="Open file", command=openfile)
-fileButton.pack(side=TOP)
+    extension_list = filename.split(".")
+    extension = extension_list[len(extension_list)-1]
+    extension = extension.replace("_Encrypted","")
+
+
+    try:
+        decrypted_data = fernet.decrypt(file_contents)
+    except:
+        messagebox.showerror(title = "ERROR", message = "Please select a valid encrypted file and key.")
+        return
+    Decrypted_file = open("Decrypted." + extension, "wb") 
+    Decrypted_file.write(decrypted_data)
+    messagebox.showinfo( "Decrypted", filename + "\n\nHas been decrypted as Decrypted." + extension)
+
+
+
+
 
 #Fernet key 
 
 #Encrypt button
-Encrypt_file = Button(root, text="Encrypt file", command = openfile)
+Encrypt_file = Button(root, text="Encrypt file", command = openfile_encrypt)
 Encrypt_file.pack(side=LEFT)
 
+#Decrypt button
+Encrypt_file = Button(root, text="Decrypt file", command = openfile_decrypt)
+Encrypt_file.pack(side=RIGHT)
 
 
 
+lbl = Label(root, font = ('Helvetica', 40, 'bold'),
+            background = 'dark slate grey',
+            foreground = 'white')
 
+def clock():
+    string = strftime('%I:%M:%S %p %b %d')
+    lbl.config(text = string)
+    lbl.after(1000, clock)
 
-
-
-
-
-
+lbl.pack(anchor = 'center')
+clock()
 
 
 
